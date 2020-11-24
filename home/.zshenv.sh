30fe7b2bcb3386fe3bf22ad3fbd3bc3eafa840e8
Sam Tolmay <sam@Sams-MacBook-Air.local>
2020-11-24 19:28:07 +0200

    remove unused

--- home/.zshenv.sh
+++ home/.zshenv.sh
@@ -1,3 +1,3 @@
 
 
-export PATH="$PATH:./node_modules:/usr/local/lib/python3.7/site-packages:$HOME/Library/Python/3.7/lib/python/site-packages::~/Library/Python/3.7/bin"
+# export PATH="$PATH"
--- terminal/startup.sh
+++ terminal/startup.sh
@@ -4,11 +4,6 @@
 # Authors:
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 
-# Set case-sensitivity for completion, history lookup, etc.
-zstyle ':prezto:*:*' case-sensitive 'no'
-
-# Color output (auto set to 'no' on dumb terminals).
-zstyle ':prezto:*:*' color 'yes'
 
 #
 # Sets directory options and defines directory aliases.
@@ -25,7 +20,6 @@ zstyle ':prezto:*:*' color 'yes'
 setopt AUTO_CD              # Auto changes to a directory without typing cd.
 setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
 setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
-setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
 setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
 setopt CDABLE_VARS          # Change directory to a path stored in a variable.
 setopt AUTO_NAME_DIRS       # Auto add variable-stored paths to ~ list.
@@ -35,31 +29,17 @@ unsetopt CLOBBER            # Do not overwrite existing files with > and >>.
                             # Use >! and >>! to bypass.
 
 #
-# Aliases
-#
-
-alias d='dirs -v'
-for index ({1..9}) alias "$index"="cd +${index}"; unset index
-
-#
 # Sets general shell options and defines environment variables.
 #
 # Authors:
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
-#
-# Smart URLs
-#
-
-autoload -Uz url-quote-magic
-zle -N self-insert url-quote-magic
 
 #
 # General
 #
 
-setopt BRACE_CCL          # Allow brace character class list expansion.
 setopt COMBINING_CHARS    # Combine zero-length punctuation characters (accents)
                           # with the base character.
 setopt RC_QUOTES          # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'.
@@ -71,33 +51,6 @@ setopt RC_QUOTES          # Allow 'Henry''s Garage' instead of 'Henry'\''s Garag
 setopt LONG_LIST_JOBS     # List jobs in the long format by default.
 setopt AUTO_RESUME        # Attempt to resume existing job before creating a new process.
 setopt NOTIFY             # Report status of background jobs immediately.
-unsetopt BG_NICE          # Don't run all background jobs at a lower priority.
-unsetopt HUP              # Don't kill jobs on shell exit.
-unsetopt CHECK_JOBS       # Don't report on jobs when shell exit.
-
-#
-# Grep
-#
-
-if zstyle -t ':prezto:environment:grep' color; then
-  export GREP_COLOR='37;45'
-  export GREP_OPTIONS='--color=auto'
-fi
-
-#
-# Termcap
-#
-
-if zstyle -t ':prezto:environment:termcap' color; then
-  export LESS_TERMCAP_mb=$'\E[01;31m'      # Begins blinking.
-  export LESS_TERMCAP_md=$'\E[01;31m'      # Begins bold.
-  export LESS_TERMCAP_me=$'\E[0m'          # Ends mode.
-  export LESS_TERMCAP_se=$'\E[0m'          # Ends standout-mode.
-  export LESS_TERMCAP_so=$'\E[00;47;30m'   # Begins standout-mode.
-  export LESS_TERMCAP_ue=$'\E[0m'          # Ends underline.
-  export LESS_TERMCAP_us=$'\E[01;32m'      # Begins underline.
-fi
-
 
 
 
@@ -134,9 +87,8 @@ setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history
 setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
 setopt HIST_BEEP                 # Beep when accessing non-existent history.
 
-#
-# Aliases
-#
+# Correct commands.
+setopt CORRECT
 
 
 #
@@ -152,112 +104,63 @@ setopt HIST_BEEP                 # Beep when accessing non-existent history.
 #   return 1
 # fi
 
-typeset -gA FX FG BG
+# typeset -gA FX FG BG
 
-FX=(
-                                        none                         "\e[00m"
-                                        normal                       "\e[22m"
-  bold                      "\e[01m"    no-bold                      "\e[22m"
-  faint                     "\e[02m"    no-faint                     "\e[22m"
-  standout                  "\e[03m"    no-standout                  "\e[23m"
-  underline                 "\e[04m"    no-underline                 "\e[24m"
-  blink                     "\e[05m"    no-blink                     "\e[25m"
-  fast-blink                "\e[06m"    no-fast-blink                "\e[25m"
-  reverse                   "\e[07m"    no-reverse                   "\e[27m"
-  conceal                   "\e[08m"    no-conceal                   "\e[28m"
-  strikethrough             "\e[09m"    no-strikethrough             "\e[29m"
-  gothic                    "\e[20m"    no-gothic                    "\e[22m"
-  double-underline          "\e[21m"    no-double-underline          "\e[22m"
-  proportional              "\e[26m"    no-proportional              "\e[50m"
-  overline                  "\e[53m"    no-overline                  "\e[55m"
+# FX=(
+#                                         none                         "\e[00m"
+#                                         normal                       "\e[22m"
+#   bold                      "\e[01m"    no-bold                      "\e[22m"
+#   faint                     "\e[02m"    no-faint                     "\e[22m"
+#   standout                  "\e[03m"    no-standout                  "\e[23m"
+#   underline                 "\e[04m"    no-underline                 "\e[24m"
+#   blink                     "\e[05m"    no-blink                     "\e[25m"
+#   fast-blink                "\e[06m"    no-fast-blink                "\e[25m"
+#   reverse                   "\e[07m"    no-reverse                   "\e[27m"
+#   conceal                   "\e[08m"    no-conceal                   "\e[28m"
+#   strikethrough             "\e[09m"    no-strikethrough             "\e[29m"
+#   gothic                    "\e[20m"    no-gothic                    "\e[22m"
+#   double-underline          "\e[21m"    no-double-underline          "\e[22m"
+#   proportional              "\e[26m"    no-proportional              "\e[50m"
+#   overline                  "\e[53m"    no-overline                  "\e[55m"
 
-                                        no-border                    "\e[54m"
-  border-rectangle          "\e[51m"    no-border-rectangle          "\e[54m"
-  border-circle             "\e[52m"    no-border-circle             "\e[54m"
+#                                         no-border                    "\e[54m"
+#   border-rectangle          "\e[51m"    no-border-rectangle          "\e[54m"
+#   border-circle             "\e[52m"    no-border-circle             "\e[54m"
 
-                                        no-ideogram-marking          "\e[65m"
-  underline-or-right        "\e[60m"    no-underline-or-right        "\e[65m"
-  double-underline-or-right "\e[61m"    no-double-underline-or-right "\e[65m"
-  overline-or-left          "\e[62m"    no-overline-or-left          "\e[65m"
-  double-overline-or-left   "\e[63m"    no-double-overline-or-left   "\e[65m"
-  stress                    "\e[64m"    no-stress                    "\e[65m"
+#                                         no-ideogram-marking          "\e[65m"
+#   underline-or-right        "\e[60m"    no-underline-or-right        "\e[65m"
+#   double-underline-or-right "\e[61m"    no-double-underline-or-right "\e[65m"
+#   overline-or-left          "\e[62m"    no-overline-or-left          "\e[65m"
+#   double-overline-or-left   "\e[63m"    no-double-overline-or-left   "\e[65m"
+#   stress                    "\e[64m"    no-stress                    "\e[65m"
 
-                                        font-default                 "\e[10m"
-  font-first                "\e[11m"    no-font-first                "\e[10m"
-  font-second               "\e[12m"    no-font-second               "\e[10m"
-  font-third                "\e[13m"    no-font-third                "\e[10m"
-  font-fourth               "\e[14m"    no-font-fourth               "\e[10m"
-  font-fifth                "\e[15m"    no-font-fifth                "\e[10m"
-  font-sixth                "\e[16m"    no-font-sixth                "\e[10m"
-  font-seventh              "\e[17m"    no-font-seventh              "\e[10m"
-  font-eigth                "\e[18m"    no-font-eigth                "\e[10m"
-  font-ninth                "\e[19m"    no-font-ninth                "\e[10m"
-)
+#                                         font-default                 "\e[10m"
+#   font-first                "\e[11m"    no-font-first                "\e[10m"
+#   font-second               "\e[12m"    no-font-second               "\e[10m"
+#   font-third                "\e[13m"    no-font-third                "\e[10m"
+#   font-fourth               "\e[14m"    no-font-fourth               "\e[10m"
+#   font-fifth                "\e[15m"    no-font-fifth                "\e[10m"
+#   font-sixth                "\e[16m"    no-font-sixth                "\e[10m"
+#   font-seventh              "\e[17m"    no-font-seventh              "\e[10m"
+#   font-eigth                "\e[18m"    no-font-eigth                "\e[10m"
+#   font-ninth                "\e[19m"    no-font-ninth                "\e[10m"
+# )
 
-FG[none]="$FX[none]"
-BG[none]="$FX[none]"
-colors=(black red green yellow blue magenta cyan white)
-for color in {0..255}; do
-  if (( $color >= 0 )) && (( $color < $#colors )); then
-    index=$(( $color + 1 ))
-    FG[$colors[$index]]="\e[38;5;${color}m"
-    BG[$colors[$index]]="\e[48;5;${color}m"
-  fi
+# FG[none]="$FX[none]"
+# BG[none]="$FX[none]"
+# colors=(black red green yellow blue magenta cyan white)
+# for color in {0..255}; do
+#   if (( $color >= 0 )) && (( $color < $#colors )); then
+#     index=$(( $color + 1 ))
+#     FG[$colors[$index]]="\e[38;5;${color}m"
+#     BG[$colors[$index]]="\e[48;5;${color}m"
+#   fi
 
-  FG[$color]="\e[38;5;${color}m"
-  BG[$color]="\e[48;5;${color}m"
-done
-unset color{s,} index
+#   FG[$color]="\e[38;5;${color}m"
+#   BG[$color]="\e[48;5;${color}m"
+# done
+# unset color{s,} index
 
-
-#
-# Sets terminal window and tab titles.
-#
-# Authors:
-#   Sorin Ionescu <sorin.ionescu@gmail.com>
-#
-
-# Return if requirements are not found.
-# if [[ "$TERM" == (dumb|linux|*bsd*) ]]; then
-#   return 1
-# fi
-
-# Sets the Terminal.app proxy icon.
-function _terminal-set-terminal-app-proxy-icon {
-  printf '\e]7;%s\a' "file://$HOST${${1:-$PWD}// /%20}"
-}
-
-# Do not override precmd/preexec; append to the hook array.
-autoload -Uz add-zsh-hook
-
-# Set up the Apple Terminal.
-function term() {
-  if [[ "$TERM_PROGRAM" == 'Apple_Terminal' ]] \
-    && ( ! [[ -n "$STY" || -n "$TMUX" || -n "$DVTM" ]] )
-  then
-    # Sets the Terminal.app current working directory before the prompt is
-    # displayed.
-    add-zsh-hook precmd _terminal-set-terminal-app-proxy-icon
-
-    # Unsets the Terminal.app current working directory when a terminal
-    # multiplexer or remote connection is started since it can no longer be
-    # updated, and it becomes confusing when the directory displayed in the title
-    # bar is no longer synchronized with real current working directory.
-    function _terminal-unset-terminal-app-proxy-icon {
-      if [[ "${2[(w)1]:t}" == (screen|tmux|dvtm|ssh|mosh) ]]; then
-        _terminal-set-terminal-app-proxy-icon ' '
-      fi
-    }
-    add-zsh-hook preexec _terminal-unset-terminal-app-proxy-icon
-
-    # Do not set the tab and window titles in Terminal.app since it sets the tab
-    # title to the currently running process by default and the current working
-    # directory is set separately.
-    return
-  fi
-}
-
-term
 
 #
 # Defines general aliases and functions.
@@ -268,82 +171,43 @@ term
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
-# Correct commands.
-setopt CORRECT
 
-#
-# Aliases
-#
 
-# Disable correction.
-alias ack='nocorrect ack'
-alias cd='nocorrect cd'
-alias cp='nocorrect cp'
-alias gist='nocorrect gist'
-alias grep='nocorrect grep'
-alias ln='nocorrect ln'
-alias man='nocorrect man'
-alias mkdir='nocorrect mkdir'
-alias mv='nocorrect mv'
-alias rm='nocorrect rm'
 
-# Disable globbing.
-alias fc='noglob fc'
-alias find='noglob find'
-alias ftp='noglob ftp'
-alias history='noglob history'
-alias locate='noglob locate'
-alias rake='noglob rake'
-alias rsync='noglob rsync'
-alias scp='noglob scp'
-alias sftp='noglob sftp'
-alias e='${(z)VISUAL:-${(z)EDITOR}}'
 
 # Checks if a name is a command, function, or alias.
-function is-callable {
-  (( $+commands[$1] )) || (( $+functions[$1] )) || (( $+aliases[$1] ))
-}
+# function is-callable {
+#   (( $+commands[$1] )) || (( $+functions[$1] )) || (( $+aliases[$1] ))
+# }
 
-if is-callable 'dircolors'; then
-  # GNU Core Utilities
-  alias ls='ls --group-directories-first'
+# if is-callable 'dircolors'; then
+#   # GNU Core Utilities
+#   alias ls='ls --group-directories-first'
 
-  if zstyle -t ':prezto:module:utility:ls' color; then
-    if [[ -s "$HOME/.dir_colors" ]]; then
-      eval "$(dircolors "$HOME/.dir_colors")"
-    else
-      eval "$(dircolors)"
-    fi
+#   if zstyle -t ':prezto:module:utility:ls' color; then
+#     if [[ -s "$HOME/.dir_colors" ]]; then
+#       eval "$(dircolors "$HOME/.dir_colors")"
+#     else
+#       eval "$(dircolors)"
+#     fi
 
-    alias ls="$aliases[ls] --color=auto"
-  else
-    alias ls="$aliases[ls] -F"
-  fi
-else
-  # BSD Core Utilities
-  if zstyle -t ':prezto:module:utility:ls' color; then
-    # Define colors for BSD ls.
-    export LSCOLORS='exfxcxdxbxGxDxabagacad'
+#     alias ls="$aliases[ls] --color=auto"
+#   else
+#     alias ls="$aliases[ls] -F"
+#   fi
+# else
+#   # BSD Core Utilities
+#   if zstyle -t ':prezto:module:utility:ls' color; then
+#     # Define colors for BSD ls.
+#     export LSCOLORS='exfxcxdxbxGxDxabagacad'
 
-    # Define colors for the completion system.
-    export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
+#     # Define colors for the completion system.
+#     export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
 
-    alias ls='ls -G'
-  else
-    alias ls='ls -F'
-  fi
-fi
-
-alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
-alias o='open'
+#     alias ls='ls -G'
+#   else
+#     alias ls='ls -F'
+#   fi
+# fi
 
 
-#
-# Functions
-#
-
-function diff {
-  git --no-pager diff --color=auto --no-ext-diff --no-index "$@"
-}
